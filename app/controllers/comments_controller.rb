93eb19b33f376f999d5690b0e3b0ead1b90c7d2b
Aissa El Mehdi <wehdi-kun@hotmail.fr>
2018-08-10 20:55:59 +0100

    Add the email of the creator and the time of creation for messages and comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,8 @@
 # Comments Controller
 class CommentsController < ApplicationController
+  before_action :find_message
+  before_action :find_comment, only: [:edit, :update, :destroy]
   def create
-    @message = Message.find(params[:message_id])
     @comment = @message.comments.create(comment_params)
     @comment.user_id = current_user.id
     @user = User.find_by_id(157)
@@ -13,12 +14,32 @@ class CommentsController < ApplicationController
   end
 
   def show
-  
+
   end
+  def edit
+  end
+  def update
+    if @comment.update(comment_params)
+      redirect_to message_path(@message)
+    else
+      render 'edit'
+    end
+  end
+  def destroy
+    @comment.destroy
+    redirect_to message_path(@message)
+  end
+
 
   private
 
   def comment_params
     params.require(:comment).permit(:content)
+  end
+  def find_message
+    @message = Message.find(params[:message_id])
+  end
+  def find_comment
+    @comment = @message.comments.find(params[:id])
   end
 end
