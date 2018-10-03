c057108468026315c58f0fc7da1998e1dece99b4
Aissa El Mehdi <wehdi-kun@hotmail.fr>
2018-10-03 17:46:49 +0100

    configure for heroku

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,9 @@ module MessageBoard
     config.log_tags  = %I[subdomain uuid]
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
 
+    # Heroku
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
